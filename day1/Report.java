d15be678e797b25e13b2fe3ad1157695aa3e902f
immkrish <92216704+immkrish@users.noreply.github.com>
2023-08-23 22:41:20 +0530

    Add files via upload

---
+++ day1/Report.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class Report {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s = new Student();
+
+		System.out.println("Student name is:" + s.studentName);
+		System.out.println("Student cgpa is:" + s.cgpa);
+		System.out.println("Student marks is:" + s.markscored);
+		System.out.println("Student Rollno is:" + s.rollno);
+		System.out.println("Student collegeName:"+s.collegeName);
+	}
+}
